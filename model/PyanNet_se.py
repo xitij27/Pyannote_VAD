a3a92d458b1610d4ab4c5934966b30ef9272ac31
xitij27 <kp27social@gmail.com>
2024-03-14 01:58:21 +0800

    PyanNet model files

---
+++ model/PyanNet_se.py
@@ -0,0 +1,220 @@
+# This code is modified from pyannote
+from typing import Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from pyannote.core.utils.generators import pairwise
+
+from pyannote.audio.core.model import Model
+from pyannote.audio.core.task import Problem, Resolution, Specifications, Task
+from pyannote.audio.models.blocks.sincnet import SincNet
+from pyannote.audio.utils.params import merge_dict
+from pyannote.audio.tasks import VoiceActivityDetection
+from pyannote.database import get_protocol
+
+import librosa
+import logging
+
+import torchaudio
+from speechbrain.pretrained import SpectralMaskEnhancement
+
+
+class PyanNet_se(Model):
+    """PyanNet_se segmentation model
+    SincNet > LSTM > Feed forward > Classifier
+    Parameters
+    ----------
+    sample_rate : int, optional
+        Audio sample rate. Defaults to 16kHz (16000).
+    num_channels : int, optional
+        Number of channels. Defaults to mono (1).
+    sincnet : dict, optional
+        Keyword arugments passed to the SincNet block.
+        Defaults to {"stride": 1}.
+    lstm : dict, optional
+        Keyword arguments passed to the LSTM layer.
+        Defaults to {"hidden_size": 128, "num_layers": 2, "bidirectional": True},
+        i.e. two bidirectional layers with 128 units each.
+        Set "monolithic" to False to split monolithic multi-layer LSTM into multiple mono-layer LSTMs.
+        This may proove useful for probing LSTM internals.
+    linear : dict, optional
+        Keyword arugments used to initialize linear layers
+        Defaults to {"hidden_size": 128, "num_layers": 2},
+        i.e. two linear layers with 128 units each.
+    """
+
+    SINCNET_DEFAULTS = {"stride": 10}
+    LSTM_DEFAULTS = {
+        "hidden_size": 128,
+        "num_layers": 2,
+        "bidirectional": True,
+        "monolithic": True,
+        "dropout": 0.0,
+    }
+    LINEAR_DEFAULTS = {"hidden_size": 128, "num_layers": 2}
+
+    def __init__(
+        self,
+        sincnet: dict = None,
+        lstm: dict = None,
+        linear: dict = None,
+        sample_rate: int = 16000,
+        num_channels: int = 1,
+        task: Optional[Task] = None,
+    ):
+
+        super().__init__(sample_rate=sample_rate, num_channels=num_channels, task=task)
+
+        sincnet = merge_dict(self.SINCNET_DEFAULTS, sincnet)
+        sincnet["sample_rate"] = sample_rate
+        self.sample_rate = sample_rate
+        lstm = merge_dict(self.LSTM_DEFAULTS, lstm)
+        lstm["batch_first"] = True
+        linear = merge_dict(self.LINEAR_DEFAULTS, linear)
+        self.save_hyperparameters("sincnet", "lstm", "linear")
+
+        # ================ load vad model ===================
+        self.sincnet = SincNet(**self.hparams.sincnet)
+        for param in self.sincnet.parameters():
+            param.requires_grad = True
+
+        # ================ load se model ===================
+        logging.info("Load se model...")
+        self.se_model = SpectralMaskEnhancement.from_hparams(
+            source="speechbrain/metricgan-plus-voicebank",
+            savedir="pretrained_models/metricgan-plus-voicebank",
+        )
+        for param in self.se_model.parameters():
+            param.requires_grad = True
+
+        monolithic = lstm["monolithic"]
+        if monolithic:
+            multi_layer_lstm = dict(lstm)
+            del multi_layer_lstm["monolithic"]
+            self.lstm = nn.LSTM(60, **multi_layer_lstm)
+
+        else:
+            num_layers = lstm["num_layers"]
+            if num_layers > 1:
+                self.dropout = nn.Dropout(p=lstm["dropout"])
+
+            one_layer_lstm = dict(lstm)
+            one_layer_lstm["num_layers"] = 1
+            one_layer_lstm["dropout"] = 0.0
+            del one_layer_lstm["monolithic"]
+
+            self.lstm = nn.ModuleList(
+                [
+                    nn.LSTM(
+                        60
+                        if i == 0
+                        else lstm["hidden_size"] * (2 if lstm["bidirectional"] else 1),
+                        **one_layer_lstm
+                    )
+                    for i in range(num_layers)
+                ]
+            )
+
+        if linear["num_layers"] < 1:
+            return
+
+        lstm_out_features: int = self.hparams.lstm["hidden_size"] * (
+            2 if self.hparams.lstm["bidirectional"] else 1
+        )
+        self.linear = nn.ModuleList(
+            [
+                nn.Linear(in_features, out_features)
+                for in_features, out_features in pairwise(
+                    [
+                        lstm_out_features,
+                    ]
+                    + [self.hparams.linear["hidden_size"]]
+                    * self.hparams.linear["num_layers"]
+                )
+            ]
+        )
+        
+        self.build()
+
+    def build(self):
+
+        if self.hparams.linear["num_layers"] > 0:
+            in_features = self.hparams.linear["hidden_size"]
+        else:
+            in_features = self.hparams.lstm["hidden_size"] * (
+                2 if self.hparams.lstm["bidirectional"] else 1
+            )
+
+        out_features = 1
+
+        self.classifier = nn.Linear(in_features, out_features)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, waveforms: torch.Tensor) -> torch.Tensor:
+        """Pass forward
+        Parameters
+        ----------
+        waveforms : (batch, channel, sample)
+        Returns
+        -------
+        scores : (batch, frame, classes)
+        """
+
+        # (16, 1, 80000) -> (16, 80000) -> (16, 1, 80000)
+        # with torch.no_grad():
+        #     enh_feat = []
+        #     for feat in batch['feat']:
+        #         enh_feat.append(se_model.audio_normalizer(feat.squeeze(0), rate))
+        #     enh_feat = torch.stack(enh_feat, dim=0).to(trainer.device)
+        #     enh_feat = se_model.enhance_batch(enh_feat, lengths=torch.tensor([1.]))
+        #
+        #     assert enh_feat.shape[-1] == batch['feat'].shape[-1], (enh_feat.shape, batch['feat'].shape)
+        #     batch['feat'] = enh_feat.unsqueeze(1)
+
+        # speech enhancement
+        self.se_model.to(waveforms.device)
+        enh_feat = []
+        for feat in waveforms:
+            enh_feat.append(self.se_model.audio_normalizer(feat.squeeze(0), self.sample_rate))
+        enh_feat = torch.stack(enh_feat, dim=0).to(waveforms.device)
+        enh_feat = self.se_model.enhance_batch(enh_feat, lengths=torch.tensor([1.], device=waveforms.device))
+        assert enh_feat.shape[-1] == waveforms.shape[-1], (enh_feat.shape, waveforms.shape)
+        waveforms = enh_feat.unsqueeze(1)
+
+        # vad
+        outputs = self.sincnet(waveforms)
+
+        if self.hparams.lstm["monolithic"]:
+            outputs, _ = self.lstm(
+                rearrange(outputs, "batch feature frame -> batch frame feature")
+            )
+        else:
+            outputs = rearrange(outputs, "batch feature frame -> batch frame feature")
+            for i, lstm in enumerate(self.lstm):
+                outputs, _ = lstm(outputs)
+                if i + 1 < self.hparams.lstm["num_layers"]:
+                    outputs = self.dropout(outputs)
+
+        if self.hparams.linear["num_layers"] > 0:
+            for linear in self.linear:
+                outputs = F.leaky_relu(linear(outputs))
+                
+        outputs = self.classifier(outputs)
+
+        return self.activation(outputs).squeeze(2)
+
+
+def test_model():
+    model = PyanNet_se(sample_rate=16000, num_channels=1)
+    
+    input_signal = torch.randn((8, 1, 16000))
+    output_model = model(input_signal)
+    
+    print(output_model.shape)
+    
+    time_st = librosa.frames_to_time(0, sr=16000, n_fft=251)
+    
+if __name__ == "__main__":
+    test_model()
